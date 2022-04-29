9692857628b6122719c20b0a1ea061ba5537f7de
bkleiner <b.kleiner@str8labs.com>
2022-04-29 22:08:20 +0200

    drv_usb: remove semaphore, reorder eps, wait for pwr

--- src/drivers/drv_usb.c
+++ src/drivers/drv_usb.c
@@ -15,10 +15,10 @@
 #include <usb_cdc.h>
 
 #define CDC_EP0_SIZE 0x08
-#define CDC_RXD_EP 0x01
+#define CDC_RXD_EP 0x02
 #define CDC_TXD_EP 0x83
 #define CDC_DATA_SZ 0x40
-#define CDC_NTF_EP 0x82
+#define CDC_NTF_EP 0x81
 #define CDC_NTF_SZ 0x08
 
 #define BUFFER_SIZE 4096
@@ -173,8 +173,6 @@ static uint32_t ubuf[0x20];
 
 static volatile bool usb_device_configured = false;
 
-static volatile bool tx_buffer_in_use = false;
-
 static uint8_t tx_buffer_data[BUFFER_SIZE];
 static circular_buffer_t tx_buffer = {
     .buffer = tx_buffer_data,
@@ -255,18 +253,14 @@ static void cdc_rxonly(usbd_device *dev, uint8_t event, uint8_t ep) {
 }
 
 static void cdc_txonly(usbd_device *dev, uint8_t event, uint8_t ep) {
-  if (tx_buffer_in_use) {
-    usbd_ep_write(dev, ep, 0, 0);
-    return;
-  }
-
   uint8_t buf[CDC_DATA_SZ];
 
-  tx_buffer_in_use = true;
   const uint32_t len = circular_buffer_read_multi(&tx_buffer, buf, CDC_DATA_SZ);
-  tx_buffer_in_use = false;
-
-  usbd_ep_write(dev, ep, buf, len);
+  if (len) {
+    usbd_ep_write(dev, ep, buf, len);
+  } else {
+    usbd_ep_write(dev, ep, 0, 0);
+  }
 }
 
 static usbd_respond cdc_setconf(usbd_device *dev, uint8_t cfg) {
@@ -339,8 +333,12 @@ void usb_init() {
   usbd_connect(&udev, true);
 
 #ifdef STM32H7
-  HAL_PWREx_EnableUSBVoltageDetector();
-  time_delay_ms(100);
+  LL_PWR_EnableUSBVoltageDetector();
+  LL_PWR_DisableUSBReg();
+
+  while (!LL_PWR_IsActiveFlag_USB()) {
+    time_delay_ms(100);
+  }
 #endif
 }
 
@@ -377,14 +375,10 @@ void usb_serial_write(uint8_t *data, uint32_t len) {
     return;
   }
 
-  while (tx_buffer_in_use)
-    __WFI();
-
   uint32_t written = 0;
   while (written < len) {
-    tx_buffer_in_use = true;
     written += circular_buffer_write_multi(&tx_buffer, data + written, len - written);
-    tx_buffer_in_use = false;
+    __WFI();
   }
 }
 
